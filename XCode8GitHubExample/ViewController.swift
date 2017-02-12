7ffb1bd4bc9408f5a055ac737b777175b1cee239
Andrew Jochl <andrewjochl@andrews-macbook-pro.local>
2017-02-12 18:42:55 -0500

    Initial Commit

---
+++ XCode8GitHubExample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  XCode8GitHubExample
+//
+//  Created by Andrew Jochl on 2/12/17.
+//  Copyright © 2017 Andrew Jochl. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
